4a1e1ba32b8c28cd2de068e745d200b41457c031
Leonardo Raduy Lemos <leonardo@mbp-de-leonardo.lan>
2022-06-16 23:18:03 -0300

    updated dist folder - outputPath no Angular.json

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/'));
+app.use(express.static('./dist/alisystem-front-prod'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/'}),
+    res.sendFile('index.html', {root: 'dist/alisystem-front-prod/'}),
 );
 
 // Start the app by listening on the default Heroku port
